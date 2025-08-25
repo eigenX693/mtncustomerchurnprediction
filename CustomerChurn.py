d0ad2b972d1b7f959efdf09af2d6f7290d391c38
e1x <fondohgracious@gmail.com>
2025-08-25 11:56:18 +0100

    Add files via upload

---
+++ CustomerChurn.py
@@ -0,0 +1,67 @@
+##This Algorithm is to model a random forest to predict Customer churning status from an MTN nigeria data
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+from sklearn.preprocessing import LabelEncoder
+import seaborn as sns
+import matplotlib.pyplot as plt
+#loading the dataset
+data = pd.read_csv(r'C:\Users\eigenX\Desktop\BACKUP\Data_Analytics\mtn_Customer_churn_Algorithm\mtn_customer_churn.csv')
+print('Data Loaded')
+##DATA CLEANING
+#truncating the unnecessary columns in the data.
+data = data.drop(['Customer ID','Full Name','Reasons for Churn'], axis=1)
+#Encoding Categorical Variables chronologically
+#Gender encoding ....Male=1,Female = 0 u gerit????
+data['Gender'] = data['Gender'].map({'Male': 1,'Female': 0})
+#customer review encoding from 1 to 5
+review_mapping = {'Poor': 1, 'Fair': 2, 'Good' : 3, 'Very Good' : 4,'Excellent' : 5}
+data['Customer Review'] = data['Customer Review'].map(review_mapping)
+#encoding the date
+data['Date of Purchase']=pd.to_datetime(data['Date of Purchase'], format='%b-%y')
+data['Date of Purchase']= data['Date of Purchase'].dt.month
+data = data.drop('Date of Purchase', axis=1)
+#the subscription plan is encoded based on the price
+#the unit price in this dataset,we donot know if its the price of the device or the price of the bundle
+plan_price_mapping = data.groupby('Subscription Plan')['Unit Price'].mean().to_dict()
+data['Subscription Plan Encoded'] = data['Subscription Plan'].map(plan_price_mapping)
+#after encoding this data,we take down the Subcription plan
+data = data.drop(['Subscription Plan'],axis = 1)
+#encoding the state and the device
+data = pd.get_dummies(data, columns = ['State','MTN Device'], drop_first= True)
+#converting the churn status to binary...churned = 1 , non churn = 0
+data['Customer Churn Status'] = data['Customer Churn Status'].map({'Yes': 1 , 'No' : 0})
+#quick check for missing values
+print(data.isnull().sum())
+data = data.fillna(method='ffill')
+print('Data Prepared for Machine Learning Algorithm: Decision Tree')
+print(data.head())
+##MACHINE LEARNING ALGORITM DEPLOYED HERE
+#Spliting data into target and features.
+X = data.drop('Customer Churn Status', axis=1)
+y = data['Customer Churn Status']
+#spliting to training and testing
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,random_state=42)
+#training 
+rf_classifier = RandomForestClassifier(n_estimators = 100,random_state = 42,class_weight='balanced')
+rf_classifier.fit(X_train, y_train)
+#testing and evaluation
+y_pred = rf_classifier.predict(X_test)
+print("\n====MODEL===EVALUATION====")
+print("\nAccuracy: ",accuracy_score(y_test,y_pred))
+print("\nConfusion Matrix: ")
+conf_matrix = confusion_matrix(y_test,y_pred)
+print(conf_matrix)
+#plotting the confusion matrix
+print("\nClassification Report: ")
+print(classification_report(y_test,y_pred))
+plt.figure(figsize=(8,6))
+sns.heatmap(conf_matrix, annot=True,fmt='d',cmap='viridis')
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Confusion Matrix')
+plt.show()
+#y_pred_proba = rf_classifier.predict_proba(X_test)[:, 1]
+#print("\nROC-AUC Score: ",roc_auc)
+
